67f0f79368e7bc633e27a6ad408deac068a59c0d
Bhaavya <bhavya@ibm.com>
2026-05-01 19:19:54 +0000

    Disable bucket-level BPA and restore public-read-write ACL

--- main.tf
+++ main.tf
@@ -6,3 +6,19 @@ resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  depends_on = [aws_s3_bucket_public_access_block.this]
+
+  bucket = aws_s3_bucket.this.id
+  acl    = "public-read-write"
+}
+
